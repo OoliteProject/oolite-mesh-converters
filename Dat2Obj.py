e721a861c2a07928779c790942289fb2b48c80e8
Marc <kaks@oolite.org>
2008-02-23 17:17:00 +0000

    Graphic converters update. Fix for Dat2Obj and added Dat2ObjTex (dat2obj plus texture conversion).
    git-svn-id: http://svn.berlios.de/svnroot/repos/oolite-linux/trunk/tools/meshConverters@1409 127b21dd-08f5-0310-b4b7-95ae10353056

--- Dat2Obj.py
+++ Dat2Obj.py
@@ -1,101 +1,95 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
-This script takes a .dat file from the Elite/Oolite source 
-and exports a .obj file containing the same geometry. 
+"""
+This script takes a .dat file from the Elite/Oolite source
+and exports a .obj file containing the same geometry.
 
-Material for the faces is set to flat white (127,127,127) 
-and surface normals need not be calculated. 
-""" 
+Material for the faces is set to flat white (127,127,127)
+and surface normals need not be calculated.
+"""
 
-import sys, string, math 
+import sys, string, math
 
-inputfilenames = sys.argv[1:] 
-print "converting..." 
-print inputfilenames 
-for inputfilename in inputfilenames: 
-   outputfilename = inputfilename.lower().replace(".dat",".obj") 
-   materialfilename = inputfilename.lower().replace(".dat",".mtl") 
-   mtllibname = string.split(materialfilename, "/")[-1] 
-   print inputfilename+"->"+outputfilename+" & "+materialfilename 
-   inputfile = open(inputfilename,"r") 
-   lines = inputfile.read().splitlines(0) 
-   outputfile = open(outputfilename,"w") 
-   materialfile = open(materialfilename,"w") 
-   materialfile.write(   '# Exported with Dat2Obj.py (C) Giles Williams 2005 - Kaks 2008\n') 
-   materialfile.write(   'newmtl default\nNs 100.000\n') 
-   materialfile.write(   'd 1.00000\nillum 2\n') 
-   materialfile.write(   'MATERIAL\t65535\t0\t0\t0\t0\t0\n') 
-   materialfile.write(   'Kd 1.00000 1.00000 1.00000\nKa 1.00000 1.00000 1.00000\n') 
-   materialfile.write(   'Ks 1.00000 1.00000 1.00000\nKe 0.00000e+0 0.00000e+0 0.00000e+0\n') 
-   materialfile.close(); 
-    
-   mode = 'SKIP' 
-   vertex_lines_out = ['# vertices...\n'] 
-   faces_lines_out = ['\g '] 
-   faces_lines_out.append ( inputfilename.lower().replace(".dat","_default")) 
-   faces_lines_out.append ('\nusemtl default\n') 
-    
-   n_verts = 0 
-   n_faces = 0 
-   skips = 0 
-   vertex=[] 
-   face=[] 
+inputfilenames = sys.argv[1:]
+print "converting..."
+print inputfilenames
+for inputfilename in inputfilenames:
+	outputfilename = inputfilename.lower().replace(".dat",".obj")
+	materialfilename = inputfilename.lower().replace(".dat",".mtl")
+	mtllibname = string.split(materialfilename, "/")[-1]
+	objname=mtllibname.replace(".mtl","")
+	print inputfilename+"->"+outputfilename+" & "+materialfilename
+	inputfile = open(inputfilename,"r")
+	lines = inputfile.read().splitlines(0)
+	
+	mode = 'SKIP'
+	vertex_lines_out = []
+	faces_lines_out = ['g '+objname+'_default\n']
+	faces_lines_out.append ('usemtl default')
+	
+	n_verts = 0
+	n_faces = 0
+	skips = 0
+	vertex=[]
 
-   for line in lines: 
-      if (mode == 'VERTEX'): 
-         coordinates = string.split(line, ',')   # split line by commas 
-         if (len(coordinates) == 3): 
-            n_verts = n_verts + 1 
-            x = float(coordinates[0]) 
-            y = float(coordinates[1]) 
-            z = float(coordinates[2]) 
-            vertex.append( (x, y, z) ) 
-            vertex_lines_out.append('v %.5f %.5f %.5f\n' % ( x, y, z)) 
-            vertex.append( (x, y, z ) ) 
-      elif (mode == 'FACES'): 
-         tokens = string.split(line, ',')   # split line by commas 
-         if (len(tokens) > 9) : 
-            color_data = tokens[0:3] 
-            normal_data =tokens[3:6] 
-            n_points = tokens[6] 
-            point_data = tokens[7:] 
-            n_faces = n_faces + 1 
-            v1 = int(point_data[0]) 
-            v2 = int(point_data[1]) 
-            v3 = int(point_data[2]) 
-            face.append((v1,v2,v3)) 
-             
-            faces_lines_out.append ('\nf ') 
-            for i in range( 0,int(n_points)) : 
-               faces_lines_out.append ('%i// ' % (int(point_data[i])+1)) 
-            # 
-             
-      elif (mode == 'SKIP'): 
-         skips = skips + 1 
-      # 
-      if (line[:6] == 'NVERTS'): 
-         mode = 'SKIP' 
-      if (line[:6] == 'NFACES'): 
-         mode = 'SKIP' 
-      if (line[:6] == 'VERTEX'): 
-         mode = 'VERTEX' 
-      if (line[:5] == 'FACES'): 
-         mode = 'FACES' 
-      # 
-   outputfile.write('# Exported with Dat2Obj.py (C) Giles Williams 2005 - Kaks 2008\n') 
-   outputfile.write('mtllib %s\n' % mtllibname) 
-   outputfile.write('o ') 
-   outputfile.write(inputfilename.lower().replace(".dat","")) 
-   outputfile.write('\n# number of vertices %d\n' % n_verts) 
-   outputfile.write('# number of faces %d\n' % n_faces) 
+	for line in lines:
+		if (mode == 'VERTEX'):
+			coordinates = string.split(line, ',')	# split line by commas
+			if (len(coordinates) == 3):
+				n_verts = n_verts + 1
+				x = -float(coordinates[0])
+				y = float(coordinates[1])
+				z = float(coordinates[2])
+				vertex.append( (x, y, z) )
+				vertex_lines_out.append('v %.5f %.5f %.5f\n' % ( x, y, z))
+				vertex.append( (x, y, z ) )
+		elif (mode == 'FACES'):
+			tokens = string.split(line, ',')	# split line by commas
+			if (len(tokens) > 9) :
+				color_data = tokens[0:3]
+				normal_data =tokens[3:6]
+				n_points = tokens[6]
+				point_data = tokens[7:]
+				n_faces = n_faces + 1
+				
+				faces_lines_out.append ('\nf ')
+				for i in range( 0,int(n_points)) :
+					faces_lines_out.append ('%i// ' % (int(point_data[i])+1))
+				#
+				
+		elif (mode == 'SKIP'):
+			skips = skips + 1
+		#
+		if (line[:6] == 'NVERTS'):
+			mode = 'SKIP'
+		if (line[:6] == 'NFACES'):
+			mode = 'SKIP'
+		if (line[:6] == 'VERTEX'):
+			mode = 'VERTEX'
+		if (line[:5] == 'FACES'):
+			mode = 'FACES'
+		#
+	outputfile = open(outputfilename,"w")
+	outputfile.write('# Exported with Dat2Obj.py (C) Giles Williams 2005 - Kaks 2008\n')
+	outputfile.write('mtllib %s\n' % mtllibname)
+	outputfile.write('o '+objname+'\n')
+	outputfile.write('# %d vertices,' % n_verts) 
+	outputfile.write(' %d faces\n' % n_faces) 
+	outputfile.writelines(vertex_lines_out)
+	outputfile.writelines(faces_lines_out)
+	outputfile.writelines('\n\n')
+	outputfile.close();
+	materialfile = open(materialfilename,"w")
+	materialfile.write(	'# Exported with Dat2Obj.py (C) Giles Williams 2005 - Kaks 2008\n')
+	materialfile.write(	'newmtl default\nNs 100.000\n')
+	materialfile.write(	'd 1.00000\nillum 2\n')
+	materialfile.write(	'MATERIAL\t65535\t0\t0\t0\t0\t0\n')
+	materialfile.write(	'Kd 1.00000 1.00000 1.00000\nKa 1.00000 1.00000 1.00000\n')
+	materialfile.write(	'Ks 1.00000 1.00000 1.00000\nKe 0.00000e+0 0.00000e+0 0.00000e+0\n\n')
+	materialfile.close();
 
-   outputfile.writelines(vertex_lines_out) 
-   outputfile.writelines(faces_lines_out) 
-                   
-   outputfile.close(); 
-print "done" 
-print "" 
-# 
-#   end 
-# 
+print "done"
+print ""
+#
+#	end
+#
---
+++ Dat2ObjTex.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python 
+
+""" 
+This script takes a .dat file from the Elite/Oolite source 
+and exports a .obj file containing the same geometry. 
+
+Material for the faces is set to flat white (127,127,127) 
+and surface normals need not be calculated. 
+""" 
+
+import sys, string, math 
+
+def tex_index(tex, vts):
+	for i in range(len(vts)):
+		if vts[i]==tex:
+			return i
+	return -1
+	
+inputfilenames = sys.argv[1:] 
+print "converting..." 
+print inputfilenames 
+for inputfilename in inputfilenames: 
+   outputfilename = inputfilename.lower().replace(".dat",".obj") 
+   materialfilename = inputfilename.lower().replace(".dat",".mtl") 
+   mtllibname = string.split(materialfilename, "/")[-1]
+   objname=mtllibname.replace(".mtl","")
+   texname=objname+'_auv'
+   texfile=''
+   print inputfilename+"->"+outputfilename+" & "+materialfilename 
+   inputfile = open(inputfilename,"r") 
+   lines = inputfile.read().splitlines(0) 
+
+   mode = 'SKIP' 
+   vertex_lines_out = [] 
+   tex_lines_out = [] 
+   faces_lines_out = ['g '+objname+'_'+texname+'\n'] 
+   faces_lines_out.append ('usemtl '+texname+'\n') 
+    
+   n_verts = 0 
+   n_faces = 0 
+   skips = 0 
+   vertex=[] 
+
+   textures=[]
+   textureCounter = 0
+   vts=[]
+   texForFace={}
+
+   for line in lines: 
+	  if (line[:8] == 'TEXTURES'): 
+		mode = 'TEXTURE' 
+	  if (mode == 'TEXTURE'):
+		tokens = string.split(line, '\t')	# split line by tabs
+		if (len(tokens) > 3):
+			if texfile == '':
+				texfile=tokens[0]
+			else:
+				if tokens[0] != texfile:
+					print 'Cannot convert .dat with more than 1 texture file. Use Dat2Obj instead.'
+					sys.exit()
+			points = tokens[2:]
+			tff=[]
+			for point in points:
+				v=string.split (point,' ')
+				vt = ('%.6f %.6f' % (float(v[0]),1-float(v[1])))
+				if not(vt in vts):
+					vts.append(vt)
+					tex_lines_out.append('vt '+vt+'\n')
+				tff[len(tff):]=[tex_index(vt, vts)]
+			texForFace[n_faces]=tff
+			#print tff
+			n_faces=n_faces + 1
+	
+   n_faces=0
+   for line in lines: 
+      if (mode == 'VERTEX'): 
+         coordinates = string.split(line, ',')   # split line by commas 
+         if (len(coordinates) == 3): 
+            n_verts = n_verts + 1 
+            x = -float(coordinates[0]) 
+            y = float(coordinates[1]) 
+            z = float(coordinates[2]) 
+            vertex.append( (x, y, z) ) 
+            vertex_lines_out.append('v %.6f %.6f %.6f\n' % ( x, y, z)) 
+            vertex.append( (x, y, z ) ) 
+      elif (mode == 'FACES'): 
+         tokens = string.split(line, ',')   # split line by commas 
+         if (len(tokens) > 9) : 
+            color_data = tokens[0:3] 
+            normal_data =tokens[3:6] 
+            n_points = tokens[6] 
+            point_data = tokens[7:] 
+             
+            faces_lines_out.append ('\nf ') 
+            for i in range( 0,int(n_points)) :
+               faces_lines_out.append ('%i/%i/ ' % (int(point_data[i])+1,texForFace[n_faces][i]+1))
+            n_faces = n_faces + 1 
+			   
+            # 
+
+      elif (mode == 'SKIP'): 
+         skips = skips + 1 
+      # 
+      if (line[:6] == 'NVERTS'): 
+         mode = 'SKIP' 
+      if (line[:6] == 'NFACES'): 
+         mode = 'SKIP' 
+      if (line[:6] == 'VERTEX'): 
+         mode = 'VERTEX' 
+      if (line[:5] == 'FACES'): 
+         mode = 'FACES' 
+      if (line[:8] == 'TEXTURES'): 
+         mode = 'TEXTURE' 
+      # 
+   outputfile = open(outputfilename,"w") 
+   outputfile.write('# Exported with Dat2ObjTex.py (C) Giles Williams 2005 - Kaks 2008\n') 
+   outputfile.write('mtllib %s\n' % mtllibname) 
+   outputfile.write('o '+objname+'\n')
+   outputfile.write('# %d vertices,' % n_verts) 
+   outputfile.write(' %d faces\n' % n_faces) 
+   outputfile.writelines(vertex_lines_out) 
+   outputfile.writelines(tex_lines_out) 
+   outputfile.writelines(faces_lines_out) 
+   outputfile.writelines('\n\n') 
+   outputfile.close(); 
+   
+   materialfile = open(materialfilename,"w") 
+   materialfile.write('# Exported with Dat2ObjTex.py (C) Giles Williams 2005 - Kaks 2008\n') 
+   materialfile.write('newmtl '+texname+'\nNs 100.000\n') 
+   materialfile.write('d 1.00000\nillum 2\n') 
+   materialfile.write('Kd 1.00000 1.00000 1.00000\nKa 1.00000 1.00000 1.00000\n') 
+   materialfile.write('Ks 1.00000 1.00000 1.00000\nKe 0.00000e+0 0.00000e+0 0.00000e+0\n') 
+   materialfile.write('map_Kd '+texfile+'\n\n') 
+   materialfile.close(); 
+print "done" 
+print "" 
+# 
+#   end 
+# 
